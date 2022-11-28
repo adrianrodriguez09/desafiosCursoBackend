aefcbea28767b8b5ce0c71f8e7473cf70ab91445
adrianrodriguez09 <103323248+adrianrodriguez09@users.noreply.github.com>
2022-11-28 18:47:47 -0300

    desafio 2 de curso de backend:CoderHouse

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,26 +1,41 @@
 const use = require("./logica.js");
 
-//TODO --> USE.PRODUCTS PARA PODER ACCEDER AL MODULO DE LOGICA Y ASÍ PODER ACCEDER A SUS MÉTODOS
+//TODO --> await use.products PARA PODER ACCEDER AL MODULO DE LOGICA Y ASÍ PODER ACCEDER A SUS MÉTODOS
 
-//* Agregar productos --> use.products.addProduct(titulo,descripción,precio,imagen,stock)
+//! Importante escribir dentro de la función llamar métodos
 
-use.products.addProduct(
-  "Mochila roja",
-  `Para llevar todo lo que quieras,pero con una mochila roja`,
-  500,
-  "https://static.dafiti.com.ar/p/jansport-9733-76539-1-product.jpg",
-  50
-);
-use.products.addProduct(
-  "Mochila azul",
-  `Para llevar todo lo que quieras,pero con una mochila azul`,
-  700,
-  "https://www.tematika.com/media/catalog/Ilhsa/Imagenes/671976.jpg",
-  20
-);
+let llamarMetodos = async () => {
+  //* Agregar productos --> use.products.addProduct(titulo,descripción,precio,imagen,stock)
 
-//* Ver todos los productos -->  use.products.getProduct()
-use.products.getProduct();
+  // await use.products.addProduct(
+  //   "Mochila roja",
+  //   `Para llevar todo lo que quieras,pero con una mochila roja`,
+  //   500,
+  //   "https://static.dafiti.com.ar/p/jansport-9733-76539-1-product.jpg",
+  //   50,
+  //   1548
+  // );
+  // await use.products.addProduct(
+  //   "Mochila azul",
+  //   `Para llevar todo lo que quieras,pero con una mochila azul`,
+  //   700,
+  //   "https://www.tematika.com/media/catalog/Ilhsa/Imagenes/671976.jpg",
+  //   20,
+  //   1546
+  // );
+  // //* Ver todos los productos -->  use.products.getProduct()
+  // await use.products.getProduct();
+  // //* Buscar producto por id --> use.products.getProductById(id)
+  // await use.products.getProductById(2);
 
-//* Buscar producto por id --> use.products.getProductById(id)
-use.products.getProductById(2);
+  //* Actualizar producto --> use.products.updateProduct(id del producto,{propiedades a cambiar: "nuevo valor de la propiedad"}), En caso de agregar más de una utilizar coma y dentro del mismo corchete
+  // await use.products.uptadeProduct(2, {
+  //   title: "Mochila marrón",
+  //   description: "Para llevar todo lo que quieras,pero con una mochila marrón",
+  // });
+
+  //* Eliminar un producto --> use.products.deleteProduct(id del producto)
+  // await use.products.deleteProduct(2)
+};
+
+llamarMetodos();
